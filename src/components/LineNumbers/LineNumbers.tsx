5ad090eef6f24ea13c9f31f0498dda710541a928
Noam Norman <normannoam21@gmail.com>
2025-03-09 17:20:19 +0200

    Adjust line number calculation for improved accuracy

--- src/components/LineNumbers/LineNumbers.tsx
+++ src/components/LineNumbers/LineNumbers.tsx
@@ -4,7 +4,7 @@ const LineNumbers: React.FC = () => {
   const [rows, setRows] = useState<number>(10);
 
   useEffect(() => {
-    const screenHeight = document.body.scrollHeight * 1.125;
+    const screenHeight = document.body.scrollHeight * 1.1;
     const lineHeight = 16 * 1.5;
     setRows(Math.floor(screenHeight / lineHeight));
   }, []);
